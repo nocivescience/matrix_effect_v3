3938d317b0b294a46e00bf9899eb3ade24f3f2e9
noceive <nocivescience@gmail.com>
2023-04-03 17:06:00 -0400

    mejoro bastante

--- script.js
+++ script.js
@@ -3,35 +3,32 @@ const ctx=matrixEl.getContext('2d');
 matrixEl.width=window.innerWidth;
 matrixEl.height=window.innerHeight;
 const widthScreen=26;
-const heightScreen=12;
+const heightScreen=2;
 columns=Math.floor(matrixEl.width/widthScreen);
 rows=Math.floor(matrixEl.height/heightScreen);
-const latin='TEAMOCLAUDIA';
+const latin='CICLOTRON';
 const rainDrop=[];
 const windDrops=[];
 for( let x=0;x<columns;x++){
     rainDrop[x]=1;
 }
-for( let y=0;y<columns;y++){
+const colors=['#0f0','#0ff','#f0f','#ff0','#f00','#00f'];
+for( let y=0;y<rows;y++){
     windDrops[y]=1
 }
 function draw(){
     ctx.fillStyle='rgba(0,0,0,0.5)';
     ctx.fillRect(0,0,matrixEl.width,matrixEl.height)
-    ctx.fillStyle='white';
-    ctx.font=`${13}px Georgia`;
+    ctx.fillStyle=colors[Math.floor(Math.random()*colors.length)];
+    ctx.font=`${Math.round(30*Math.random())}px Georgia`;
     for(let i=0;i<rainDrop.length;i++){
         let random=Math.floor(Math.random()*rainDrop.length)
-        ctx.fillText(latin[Math.floor(Math.random()*latin.length)],i*widthScreen,widthScreen*rainDrop[i]*2+random*4);
-        rainDrop[i]++;
+        ctx.fillText(latin[Math.floor(Math.random()*latin.length)],i*widthScreen,heightScreen*rainDrop[i]*30+random*4);
+        rainDrop[i]+=.4;
         if(rainDrop[i]*widthScreen*2>matrixEl.height){
             rainDrop[i]=1;
             random=undefined;
         }
     }
 }
-function horizontal(){
-    ctx.fillStyle='rgba(0,0,0,.05)';
-    
-}
-setInterval(()=>{draw()},100)
+setInterval(draw,100)
